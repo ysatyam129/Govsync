1dec6bd0fd449ccd54f49b9bbfb9afc710e31c45
SATYAM SINGH YADAV <146921177+ysatyam129@users.noreply.github.com>
2025-06-28 04:58:28 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 // const mongo_url = process.env.MONGO_CONN;
-// console.log("MongoDB Connection URL:", mongodb+srv://satendrakaushik2002:KRn7NUIYkUva9IQ7@govsync.odt1h.mongodb.net/?retryWrites=true&w=majority&appName=GovSync);
+// console.log("");
 // mongoose.connect(mongo_url)
 //     .then(() => {
 //         console.log("MongoDB Connected...");
